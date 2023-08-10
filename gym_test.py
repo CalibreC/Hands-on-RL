514aa195fad54f2b8da2e1217d5070573e8f914f
MISday <47630005+MISday@users.noreply.github.com>
2023-08-10 11:09:24 +0800

    DQN train_test

--- gym_test.py
+++ gym_test.py
@@ -1,6 +1,7 @@
 import gym
-env = gym.make("CartPole-v1")
+env = gym.make("CartPole-v0")
 env.reset()
 for _ in range(1000):
     env.render()
     env.step(env.action_space.sample())
+    print("OK")
